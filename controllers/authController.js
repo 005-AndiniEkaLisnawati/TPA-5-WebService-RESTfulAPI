ce0ed1ae0ec8afc266511fabd22a6ded5574dbe0
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-10-17 14:24:23 +0700

    feat: add route auth functionally

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,34 @@
+const User = require('../models/user.model');
+
 module.exports = {
     register: (req, res) => {
         try {
-            const {name, email, password} = req.body;
-        if(!name|!email|!password){
-            
-        }
+           const {name, email, password} = req.body;
+           const newUser = new User({name, email, password});
+           newUser.save();
+           res.status(201).json({
+               message: "User registered successfully",
+               user: newUser
+           });
 
         } catch (e) {
-            
+            res.status(500).json({message: "Registration failed", error: e.message});
         
         
         }
     },
-    login: (req, res) => {
 
+    login: async(req, res) => {
+        try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email, password});
+            if(!user){
+                return res.status(401).json({message: "Invalid email or password"});
+            }  
+            res.status(200).json({message: "Login succesfully!!"})
+        }
+        catch(err){
+            res.status(500).json({message: "Login failed", error: err.message});
+        }
     }
 }
